5affb7d5a531c29b3bc3e66532d5e24cbca5ca34
ASmokeyPie <mattwalker94@hotmail.com>
2025-10-03 17:27:05 +0100

    Test
    Signed-off-by: ASmokeyPie <mattwalker94@hotmail.com>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,12 +8,10 @@ public class App {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 4; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            
-
         }
     }
 }
